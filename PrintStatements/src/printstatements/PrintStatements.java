33d042dbb3947eefc972c573528e4add2d1d20b5
khylaehza <khondrade@gmail.com>
2024-02-16 15:03:15 +0800

    feat:  semester 1

---
+++ PrintStatements/src/printstatements/PrintStatements.java
@@ -0,0 +1,35 @@
+/***********************************************************************************************************************************
+*   Author: Khyla Ehza Hondrade 
+*   Activity Type: 02 Task Performance 1
+*
+*   Program Name: PrintStatements.java
+*   Description: A program that uses println() statements to draw an item.
+*   Date Created: 10/03/2020
+***********************************************************************************************************************************/
+
+package printstatements;
+
+public class PrintStatements {
+    public static void main(String args[]) {
+      //Prints an eyeglasses
+      //Notice that backward slash(\\) is double since using one(\) will throw an illegal escape character error.
+      System.out.println("            _                                           _");
+      System.out.println("           / \\                                         / \\ "); 
+      System.out.println("          /   \\                                       /   \\ ");
+      System.out.println("         /     \\                                     /     \\ ");
+      System.out.println("        /       \\                                   /       \\ ");
+      System.out.println("       /   /\\    \\                                 /    /\\   \\ ");
+      System.out.println("      /   /  \\    \\                               /    /  \\   \\ ");
+      System.out.println("     /   /    \\___/                               \\___/    \\   \\ ");
+      System.out.println("    /   /                                                   \\   \\ ");
+      System.out.println("   /   /                                                     \\   \\ ");
+      System.out.println("  /   /                                                       \\   \\ ");
+      System.out.println(" /__ /_________________________       _________________________\\ __\\ ");
+      System.out.println("|    ________________________  \\_____/  ________________________    | ");
+      System.out.println("|_  \\                       /  .----.  \\                       /   _| ");
+      System.out.println("  \\  \\		            /  /	\\  \\                     /  /  ");
+      System.out.println("   \\  \\___________________/  /        \\  \\___________________/  /	");
+      System.out.println("    \\_______________________/          \\_______________________/ ");
+      
+    }
+}
